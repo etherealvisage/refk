cac363441f4ecd5d03dd616e97ee57ec454fa083
ethereal <ethereal@ethv.net>
2016-03-05 00:40:13 -0800

    Updated .gitignore.

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-TERM=xterm qemu-system-x86_64 -m 64 -curses -kernel kernel/kernel.bin -d int,cpu_reset 2>output -serial file:serial
+TERM=xterm qemu-system-x86_64 -m 64 -curses -kernel kernel/kernel.bin -d int,cpu_reset 2>ints -serial file:serial
