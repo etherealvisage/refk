cd7d94d9418e1c69a35ef3e54b89379af0ecb4e9
ethereal <ethereal@ethv.net>
2016-03-06 21:14:27 -0800

    Successfully remotely spawned two tasks.

--- kernel/scheduler/scheduler.c
+++ kernel/scheduler/scheduler.c
@@ -3,6 +3,7 @@
 #include "klib/desc.h"
 #include "klib/kcomm.h"
 #include "klib/lapic.h"
+#include "klib/kmem.h"
 
 #include "scheduler.h"
 
@@ -11,16 +12,8 @@ char tick_stack[1024];
 static void tick(uint64_t vector, uint64_t excode, task_state_t *ret_task) {
     void (*transfer)(uint64_t, task_state_t *) = (void *)0xffffffffffe00000;
 
-    d_printf("tick!\n");
-
     if(ret_task->state & TASK_STATE_RUNNABLE) {
-        d_printf("Previous task was runnable, so should swap to new runnable!\n");
-
-        d_printf("ret_task: %x\n", ret_task);
-        d_printf("TASK_MEM(0): %x\n", TASK_MEM(0));
         int64_t init = ret_task - TASK_MEM(0);
-
-        d_printf("Originally at index %x\n", init);
 
         task_state_t *nts = ret_task;
         for(int off = 1; off < NUM_TASKS; off ++) {
@@ -32,15 +25,15 @@ static void tick(uint64_t vector, uint64_t excode, task_state_t *ret_task) {
                 continue;
             }
 
-            d_printf("Switching to index %x\n", in);
             nts = TASK_MEM(in);
             break;
         }
 
         ret_task = nts;
     }
-
-    d_printf("Switching to task %x\n", ret_task);
+    else {
+        d_printf("Not in runnable task!\n");
+    }
 
     lapic_conditional_eoi(vector);
     transfer(0, ret_task);
@@ -66,6 +59,97 @@ static void t2() {
     }
 }
 
+char spawner_stack[1024];
+
+static void spawner() {
+    __asm__("sti");
+    d_printf("Spawning!\n");
+
+    kcomm_t *schedin = (void *)COMM_BASE_ADDRESS;
+    kcomm_t *schedout = (void *)COMM_BASE_ADDRESS + COMM_OUT_OFFSET;
+
+    /* Spawn t1 thread */
+    comm_in_packet_t in;
+    in.type = COMM_SPAWN;
+    in.spawn.cr3 = kmem_current();
+
+    kcomm_put(schedin, &in, sizeof(in));
+
+    comm_out_packet_t out;
+    uint64_t result_size;
+    while(kcomm_get(schedout, &out, &result_size)) ;
+
+    task_state_t *ts = out.spawn.task;
+    in.type = COMM_SET_STATE;
+    in.set_state.state = ts;
+#define SET(index_, value_) \
+    in.set_state.index = index_; \
+    in.set_state.value = (uint64_t)value_; \
+    kcomm_put(schedin, &in, sizeof(in));
+
+    SET(COMM_STATE_RIP, t1)
+    SET(COMM_STATE_RSP, t1_stack + 1024)
+    SET(COMM_STATE, TASK_STATE_VALID | TASK_STATE_RUNNABLE);
+
+    d_printf("Spawned t1 thread.\n");
+
+    /* Spawn t2 thread */
+    in.type = COMM_SPAWN;
+    in.spawn.cr3 = kmem_current();
+
+    kcomm_put(schedin, &in, sizeof(in));
+
+    while(kcomm_get(schedout, &out, &result_size)) ;
+
+    ts = out.spawn.task;
+    in.type = COMM_SET_STATE;
+    in.set_state.state = ts;
+
+    SET(COMM_STATE_RIP, t2)
+    SET(COMM_STATE_RSP, t2_stack + 1024)
+    SET(COMM_STATE, TASK_STATE_VALID | TASK_STATE_RUNNABLE);
+
+    d_printf("Spawned t2 thread.\n");
+
+    while(1) {}
+}
+
+void listen() {
+    d_printf("Listening!\n");
+    kcomm_t *schedin = (void *)COMM_BASE_ADDRESS;
+    kcomm_t *schedout = (void *)COMM_BASE_ADDRESS + COMM_OUT_OFFSET;
+
+    comm_in_packet_t in;
+    while(1) {
+        uint64_t in_size;
+        if(kcomm_get(schedin, &in, &in_size)) continue;
+
+        switch(in.type) {
+        case COMM_FORWARD: {
+            break;
+        }
+        case COMM_SPAWN: {
+            task_state_t *ts = task_create();
+            ts->cr3 = in.spawn.cr3;
+            comm_out_packet_t out;
+            out.type = COMM_SPAWN;
+            out.spawn.task = ts;
+            kcomm_put(schedout, &out, sizeof(out));
+            break;
+        }
+        case COMM_SET_STATE: {
+            task_state_t *ts = in.set_state.state;
+            uint64_t *indexed = (void *)ts;
+            indexed[in.set_state.index] = in.set_state.value;
+            break;
+        }
+        default:
+            d_printf("Unknown comm_in packet type!\n");
+            break;
+        }
+    }
+}
+
 void _start() {
     d_printf("scheduler!\n");
     lapic_setup();
@@ -82,20 +166,22 @@ void _start() {
     task_state_t *tick_ts = task_create();
     task_set_local(tick_ts, tick, tick_stack + 1024);
 
-    d_printf("setting IRQ for 0x82 to 0x%x\n", tick_ts);
     DESC_INT_TASKS_MEM[0x82] = (uint64_t)tick_ts;
 
-    task_state_t *t1_task = task_create();
-    task_set_local(t1_task, t1, t1_stack + 1024);
-    task_mark_runnable(t1_task);
-    task_state_t *t2_task = task_create();
-    task_set_local(t2_task, t2, t2_stack + 1024);
-    task_mark_runnable(t2_task);
+    task_state_t *spawner_task = task_create();
+    task_set_local(spawner_task, spawner, spawner_stack + 1024);
+    task_mark_runnable(spawner_task);
+
+    task_state_t *self_task = task_create();
+    self_task->state = TASK_STATE_VALID | TASK_STATE_RUNNABLE;
+    //task_mark_runnable(self_task);
 
     __asm__("sti");
     
-    void (*transfer)(uint64_t, task_state_t *) = (void *)0xffffffffffe00000;
-    transfer(0, t1_task);
+    void (*transfer)(task_state_t *, task_state_t *) = (void *)0xffffffffffe00000;
+    transfer(self_task, self_task);
+
+    listen();
 
     while(1) {}
 }
--- kernel/scheduler/scheduler.h
+++ kernel/scheduler/scheduler.h
@@ -11,6 +11,38 @@
 enum {
     COMM_FORWARD,
     COMM_SPAWN,
+    COMM_SET_STATE,
+};
+
+enum {
+    COMM_STATE_RAX,
+    COMM_STATE_RBX,
+    COMM_STATE_RCX,
+    COMM_STATE_RDX,
+    COMM_STATE_RSI,
+    COMM_STATE_RDI,
+    COMM_STATE_RSP,
+    COMM_STATE_RBP,
+    COMM_STATE_R8,
+    COMM_STATE_R9,
+    COMM_STATE_R10,
+    COMM_STATE_R11,
+    COMM_STATE_R12,
+    COMM_STATE_R13,
+    COMM_STATE_R14,
+    COMM_STATE_R15,
+    COMM_STATE_RFLAGS,
+    COMM_STATE_RIP,
+    COMM_STATE_CS,
+    COMM_STATE_DS,
+    COMM_STATE_ES,
+    COMM_STATE_FS,
+    COMM_STATE_GS,
+    COMM_STATE_SS,
+    COMM_STATE_FS_BASE,
+    COMM_STATE_GS_BASE,
+    COMM_STATE_CR3,
+    COMM_STATE
 };
 
 typedef struct comm_in_packet_t {
@@ -22,8 +54,13 @@ typedef struct comm_in_packet_t {
             char message[32];
         } forward;
         struct {
-            
+            uint64_t cr3;
         } spawn;
+        struct {
+            task_state_t *state;
+            uint64_t index;
+            uint64_t value;
+        } set_state;
     };
 } comm_in_packet_t;
 
