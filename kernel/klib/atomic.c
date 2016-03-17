b775f7e60983685a4d0d197ca6ff92e28e630707
ethereal <ethereal@ethv.net>
2016-03-16 19:27:52 -0700

    Documented some conventions and added atomic op code.

---
+++ kernel/klib/atomic.c
@@ -0,0 +1,61 @@
+#include "atomic.h"
+
+#define CACHE_LINE_SIZE 64
+#define CACHE_LINE_MASK (CACHE_LINE_SIZE-1)
+#define CACHE_LINE_MAX (CACHE_LINE_SIZE-7)
+
+#define CACHE_LINE_WITHIN(a) \
+    ((((uint64_t)a) & (CACHE_LINE_MASK)) >= CACHE_LINE_MAX)
+
+void atomic_write(uint64_t *address, uint64_t value) {
+    if(CACHE_LINE_WITHIN(address)) {
+        *address = value;
+    }
+    else {
+        __asm__ __volatile__(
+            "lock   xchg %%rbx, 0(%%rax)"
+            :
+            : "a"(address), "b"(value));
+    }
+}
+
+uint64_t atomic_read(uint64_t *address) {
+    if(CACHE_LINE_WITHIN(address)) {
+        return *address;
+    }
+    else {
+        uint64_t value;
+        __asm__ __volatile__(
+            "lock   addq %%rbx, 0(%%rax)"
+            : "=b"(value)
+            : "a"(address), "b"(0));
+        return value;
+    }
+}
+
+void atomic_inc(uint64_t *address) {
+    __asm__ __volatile__(
+        "lock incq 0(%%rax)"
+        :
+        : "a"(address));
+}
+
+void atomic_dec(uint64_t *address) {
+    __asm__ __volatile__(
+        "lock decq 0(%%rax)"
+        :
+        : "a"(address));
+}
+
+uint64_t atomic_swap(uint64_t *address, uint64_t value) {
+    __asm__ __volatile__(
+        "lock xchgq 0(%%rax), %%rbx"
+        : "=b"(value)
+        : "a"(address), "b"(value));
+
+    return value;
+}
+
+int atomic_swapcompare(uint64_t *address, uint64_t expected, uint64_t value) {
+    return 1;
+}
---
+++ kernel/klib/atomic.h
@@ -0,0 +1,18 @@
+#ifndef KLIB_ATOMIC_H
+#define KLIB_ATOMIC_H
+
+#include <stdint.h>
+
+// NOTE: all atomic operations are on qwords
+
+void atomic_write(uint64_t *address, uint64_t value);
+uint64_t atomic_read(uint64_t *address);
+
+void atomic_inc(uint64_t *address);
+void atomic_dec(uint64_t *address);
+
+uint64_t atomic_swap(uint64_t *address, uint64_t value);
+
+int atomic_swapcompare(uint64_t *address, uint64_t expected, uint64_t value);
+
+#endif
